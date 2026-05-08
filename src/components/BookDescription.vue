ef5329835f75c576e50f189fcfd3ee5951ac3d2c
aliashleyg <allisonguthrie1980@gmail.com>
2026-05-08 05:10:30 -0600

    expanded description toggle functionality added to :

--- src/components/BookDescription.vue
+++ src/components/BookDescription.vue
@@ -1,13 +1,38 @@
 <script setup>
+import {ref} from "vue";
 defineProps({
   description: String,
 })
+
+const isDescriptionExpanded = ref(false)
+
+function expandDescription(description) {
+  isDescriptionExpanded.value = !isDescriptionExpanded.value
+  console.log(isDescriptionExpanded.value)
+}
+
 </script>
 
 <template>
-  <p>{{description}}</p>
+  <p v-if="isDescriptionExpanded">
+    {{description}}
+    <span class="pseudoLinkStyle" @click="expandDescription()">CLOSE</span>
+  </p>
+  <p v-else>
+    {{description.slice(0,175)}}...
+    <span class="pseudoLinkStyle" @click="expandDescription()">MORE</span>
+  </p>
 </template>
 
 <style scoped>
+.pseudoLinkStyle {
+  color: blue;
+  text-decoration: underline;
+}
+
+.pseudoLinkStyle:hover {
+  color: gray;
+  text-decoration: underline;
+}
 
 </style>
